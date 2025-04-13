038424bf8aa047b835297d1b20f56fbc4db35aef
Ankit Sharma <ankit.sharma@filuet.com>
2025-04-13 23:41:17 +0530

    feat: add redux store and theme configuration to root file

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,9 +1,18 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/rootStore';
+import ThemeCustomization from './theme';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Root element not found');
+} else {
+  createRoot(rootElement).render(
+    <Provider store={store}>
+      <ThemeCustomization>
+        <App />
+      </ThemeCustomization>
+    </Provider>
+  );
 }
-
-createRoot(rootElement).render(<App />);
